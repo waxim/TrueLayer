4a65a0dc62d7cf3b3aac66bdbf9e13f21a8a4b7a
Charlie J <charliefjennings+git@gmail.com>
2019-03-20 17:18:54 +0000

    Update PendingTransactions.php

--- src/Bank/Card/PendingTransactions.php
+++ src/Bank/Card/PendingTransactions.php
@@ -22,7 +22,8 @@ class PendingTransactions extends Request
             ->setAccessToken($this->token->getAccessToken())
             ->get("/data/v1/cards/" . $account_id . "/transactions/pending");
 
-        $this->OAuthCheck($result);$data = json_decode($result->getBody(), true);
+        $this->OAuthCheck($result);
+        $data = json_decode($result->getBody(), true);
         $results = array_walk($data['results'], function ($value) {
             return new CardTransaction($value);
         });
